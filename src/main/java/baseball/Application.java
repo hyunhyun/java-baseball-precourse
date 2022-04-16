18942a751155b809d287c904f5cc4b1185b9145b
hyun <jhyun_yi@naver.com>
2022-04-16 10:47:29 +0900

    feat : 게임 반복 수행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import baseball.constant.BaseBallMessage;
+import baseball.play.PlayGame;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayGame playGame = new PlayGame();
+
+        int retryGame = 1;
+        while (retryGame == 1) {
+            playGame.gameRound(); //한문제로 게임 1round 진행
+            System.out.println(BaseBallMessage.GUIDE_MSG_REGAME);
+            retryGame = Integer.parseInt(Console.readLine());
+            if(retryGame == 2){
+                break;
+            }
+        }
     }
 }
