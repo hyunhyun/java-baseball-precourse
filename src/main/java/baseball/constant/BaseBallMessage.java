07cb2d60c61ab19d590ff2a7ac7a36a8d286bdb2
hyun <jhyun_yi@naver.com>
2022-04-16 10:43:51 +0900

    feat(BaseBallMessage) : 메시지 상수 추가

---
+++ src/main/java/baseball/constant/BaseBallMessage.java
@@ -0,0 +1,12 @@
+package baseball.constant;
+
+public final class BaseBallMessage {
+    public static final String OUTPUT_MSG_BOTH = "%d볼 %d스트라이크";
+    public static final String OUTPUT_MSG_BALL = "%d볼";
+    public static final String OUTPUT_MSG_STRIKE = "%d스트라이크";
+    public static final String OUTPUT_MSG_NOTHING = "낫싱";
+    public static final String OUTPUT_MSG_SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String GUIDE_MSG_INPUT = "숫자를 입력해주세요 : ";
+    public static final String GUIDE_MSG_REGAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
